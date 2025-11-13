c36de33767c024c3815aa0d1bd95ed9eaa04a1a7
Ade Rizky <aderizkyfaqi@gmail.com>
2025-11-13 09:24:04 +0700

    add url railway

--- src/application/swagger.js
+++ src/application/swagger.js
@@ -11,7 +11,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
+      url: "https://financial-transaction-api-production.up.railway.app",
       description: "Development Server",
     },
   ],
